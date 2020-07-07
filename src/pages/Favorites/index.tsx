f5c7b6d7a251a2479b83f4ce5abdc030237306fa
Matheus Neves <matheusneves1989@gmail.com>
2020-07-06 21:39:48 -0300

    feat: implementing page Favorites

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,7 +32,14 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const response = await api.get<Food[]>('favorites');
+
+      const serializedData = response.data.map(food => ({
+        ...food,
+        formattedPrice: formatValue(food.price),
+      }));
+
+      setFavorites(serializedData);
     }
 
     loadFavorites();
